d0a818b779ddab100209f7be1b1e5c3583bc5c2c
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-10 18:59:18 +0530

    Added : Dictionary and percentage for singlet

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -2,11 +2,32 @@
 
 echo "WELCOME TO FLIP COIN SIMULATION"
 
-randomFlip=$((RANDOM%2))
+read -p "enter the number of times you want to flip the coin" flip
 
-if [ $randomFlip -eq 1 ]
-then
-	echo "Head"
-else
-	echo "tail"
-fi
+#variables
+head=0
+tail=0
+
+declare -A combinationDict
+
+function singletValue()
+{
+	for((i=1;i<=flip;i++))
+	do
+		randomFlip=$((RANDOM%2))
+		if [ $randomFlip -eq 1 ]	
+		then
+			head=$(($head+1))
+			combinationDict[FlipCoin$i]=Head
+		else
+			tail=$(($tail+1))
+			combinationDict[FlipCoin$i]=Tail
+		fi
+	done
+
+	total=$(($head+$tail))
+	headPer=$((100*$head/$total))
+	tailPer=$((100*$tail/$total))
+	echo $headPer
+}
+singletValue
