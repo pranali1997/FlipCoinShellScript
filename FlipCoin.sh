abb39d90f04196b54b2edefcaaf820d09faa7805
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-11 10:18:18 +0530

    Modified : added main function

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -14,40 +14,42 @@ declare -A combinationDict
 
 function getValues()
 {
-        for (( j=0; j<$flip; j++ ))
-        do
-                unset coinValue
-                for (( i=0; i<$coin; i++ ))
-                do
-                        random=$((RANDOM%2))
-                        if [ $random -eq 1 ]
-                        then
-                                coinValue="H$coinValue "
-                                head=$(($head+1))
-                        else
-                                coinValue="T$coinValue"
-                                tail=$(($tail+1))
-                        fi
-                done
-                combinationDict[$coinValue]=$(( ${combinationDict[$coinValue]} + 1 ))
-                value="${!combinationDict[@]}"
-        done
-        total=$flip
-        echo ${combinationDict[@]}
+	for (( j=0; j<$flip; j++ ))
+	do
+		coinValue=""
+		for (( i=0; i<$coin; i++ ))
+		do
+			random=$((RANDOM%2))
+			if [ $random -eq 1 ]
+			then
+				coinValue="H$coinValue "
+				head=$(($head+1))
+			else
+				coinValue="T$coinValue"
+				tail=$(($tail+1))
+			fi
+		done
+		combinationDict[$coinValue]=$(( ${combinationDict[$coinValue]} + 1 ))
+	done
+	total=$flip
+	echo ${combinationDict[@]}
 }
 
 function getPercentage()
 {
-	 counter=0
-    values=$(getValues $coin)
-   for i in $values
-   do
-      counter=$(($counter+1))
-      resultDict[$counter]=$((100*$i/flip))
-   done
-   echo ${resultDict[@]}
+	counter=0
+	values=$(getValues $coin)
+	for i in $values
+	do
+		counter=$(($counter+1))
+		resultDict[$counter]=$((100*$i/flip))
+	done
+	echo ${resultDict[@]}
 }
 
-result=$(getPercentage)
-echo "percentage of $coin coin " $( printf "%s\n" $result | sort -n )
-echo "winner of $coin coin " $( printf "%s\n" $result | sort -n | tail -1 )
+function main()
+{
+	result=$(getPercentage)
+	echo "winner of $coin coin " $( printf "%s\n" $result  | sort -n | tail -1 )
+}
+main
